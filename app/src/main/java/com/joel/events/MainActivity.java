9997714822e148d40e1209294b9fd2019e6d0c0a
Joelmukono <jlmukono@gamil.com>
2020-01-31 13:05:50 +0300

    Create main app activity and add functionality to click a button and move to next activity

---
+++ app/src/main/java/com/joel/events/MainActivity.java
@@ -0,0 +1,35 @@
+package com.joel.events;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG  = MainActivity.class.getSimpleName();
+    @BindView(R.id.openAddEventsButton) Button mOpenEventsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mOpenEventsButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        if(v == mOpenEventsButton){
+            Intent intent = new Intent(MainActivity.this, AddEvents.class);
+            startActivity(intent);
+        }
+    }
+
+
+}
