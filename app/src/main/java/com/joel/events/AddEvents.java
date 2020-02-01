ffb1d1a865dfc0986267a8c4d0a4609e147b6aaf
Joelmukono <jlmukono@gamil.com>
2020-02-01 14:16:00 +0300

    convert mEeventtext to string

--- app/src/main/java/com/joel/events/AddEvents.java
+++ app/src/main/java/com/joel/events/AddEvents.java
@@ -2,6 +2,7 @@ package com.joel.events;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +27,10 @@ public class AddEvents extends AppCompatActivity implements View.OnClickListener
 
     @Override
     public void onClick(View v){
+        if(v == mAddEvent){
+            String event = mEventText.getText().toString();
+
+        }
 
     }
 }
