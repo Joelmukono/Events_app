719ee0ed60b8cdf813fcc0bcd08cf2fe497a4b75
Joelmukono <jlmukono@gamil.com>
2020-02-02 20:20:44 +0300

    add logic for eventAdapter

--- app/src/main/java/com/joel/events/EventsAdapter.java
+++ app/src/main/java/com/joel/events/EventsAdapter.java
@@ -1,4 +1,26 @@
 package com.joel.events;
+import android.content.Context;
 
-public class EventsAdapter {
+import android.widget.ArrayAdapter;
+public class EventsAdapter extends ArrayAdapter {
+
+    private Context mContext;
+    private String[] mEvents;
+
+    public EventsAdapter(Context mContext, int resource, String[] mEvents){
+        super(mContext,resource);
+        this.mContext = mContext;
+        this.mEvents = mEvents;
+    }
+
+    @Override
+    public Object getItem(int position){
+        String event = mEvents[position];
+        return String.format("%s \nAttend",event);
+    }
+
+    @Override
+    public int getCount(){
+        return mEvents.length;
+    }
 }
