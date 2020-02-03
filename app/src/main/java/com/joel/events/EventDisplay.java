76796a88e15f33081effe82daba7cdafdab47d24
Joelmukono <jlmukono@gamil.com>
2020-02-03 07:23:26 +0300

    add logic for passing data between activities

--- app/src/main/java/com/joel/events/EventDisplay.java
+++ app/src/main/java/com/joel/events/EventDisplay.java
@@ -5,16 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ListView;
 
 public class EventDisplay extends AppCompatActivity {
+
+    @BindView(R.id.eventsText) TextView mEventsText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_display);
+
+        ButterKnife.bind(this);
+
         Intent intent = getIntent();
         String events = intent.getStringExtra("event");
+        mEventsText.setText("Event name is :" + events);
 
 
     }
