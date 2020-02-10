7820c42756b03225a874a7eead816c2a07aabcd9
Joelmukono <jlmukono@gamil.com>
2020-02-10 09:37:10 +0300

    add events clients class

--- app/src/main/java/com/joel/events/services/EventsClient.java
+++ app/src/main/java/com/joel/events/services/EventsClient.java
@@ -1,4 +1,19 @@
 package com.joel.events.services;
 
+import com.joel.events.interfaces.EventsApi;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class EventsClient {
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient(String baseUrl){
+        if(retrofit==null){
+            retrofit = new Retrofit.Builder().baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit;
+    }
+
+
 }
