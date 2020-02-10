efbf4e92430fb34e82d653d6912469529cb5399c
Joelmukono <jlmukono@gamil.com>
2020-02-10 08:46:14 +0300

    add api key to constants

---
+++ app/src/main/java/com/joel/events/Constants.java
@@ -0,0 +1,6 @@
+package com.joel.events;
+
+public class Constants {
+    public static final String EVENT_BASE_URL = " https://www.eventbriteapi.com/v3/categories";
+    public static final String EVENT_API_KEY = BuildConfig.EVENT_API_KEY;
+}
