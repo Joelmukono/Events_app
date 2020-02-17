a92524d43a24af657c114db87c68f595c2f2e848
Joelmukono <jlmukono@gamil.com>
2020-02-17 12:49:51 +0300

    add event view holder class

--- app/src/main/java/com/joel/events/adapters/FirebaseEventViewHolder.java
+++ app/src/main/java/com/joel/events/adapters/FirebaseEventViewHolder.java
@@ -1,4 +1,11 @@
 package com.joel.events.adapters;
 
-public class FirebaseEventViewHolder {
+import android.content.Context;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FirebaseEventViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    View mView;
+    Context mContext;
 }
