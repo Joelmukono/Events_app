ff9ba2c89e40acd574ea52dab0a4c3ab94362a45
Joelmukono <jlmukono@gamil.com>
2020-02-19 08:10:39 +0300

    create login activity

--- app/src/main/java/com/joel/events/ui/LoginActivity.java
+++ app/src/main/java/com/joel/events/ui/LoginActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.joel.events.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_account);
+        setContentView(R.layout.activity_login);
     }
 }
